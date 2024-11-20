137bfa0068c4a00147dfd6c2afadef7084aecc07
toxuannghia <huyanbeo123@gmail.com>
2024-11-21 00:44:10 +0700

    nghia init project

---
+++ CafeCMU2/Managing coffee shop/Managing_coffee_shop.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using Managing_coffee_shop.Blazor.Server.Services;
+
+namespace Managing_coffee_shop.Blazor.Server;
+
+public class Managing_coffee_shopBlazorApplication : BlazorApplication {
+    public Managing_coffee_shopBlazorApplication() {
+        ApplicationName = "Managing coffee shop";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += Managing_coffee_shopBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void Managing_coffee_shopBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
