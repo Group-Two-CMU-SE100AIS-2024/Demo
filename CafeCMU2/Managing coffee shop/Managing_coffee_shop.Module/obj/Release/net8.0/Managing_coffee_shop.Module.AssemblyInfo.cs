06086fade706826b66053853f2988eabeb32809e
toxuannghia <huyanbeo123@gmail.com>
2024-11-21 07:40:20 +0700

    nghia cap nhat luc 7h40

--- CafeCMU2/Managing coffee shop/Managing_coffee_shop.Module/obj/Release/net8.0/Managing_coffee_shop.Module.AssemblyInfo.cs
+++ CafeCMU2/Managing coffee shop/Managing_coffee_shop.Module/obj/Release/net8.0/Managing_coffee_shop.Module.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Managing_coffee_shop.Module")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8db72b5f80bf83c850662efcbb181364a789d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Managing_coffee_shop.Module")]
